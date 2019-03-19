aef412b8001c30849b475d0db7220130b85629e8
anksite <adnankurniawan98@gmail.com>
2019-03-19 14:56:13 +0700

    commit awal

---
+++ Class dan Object/Mahasiswa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_dan_Object {
+    class Mahasiswa {
+
+        //properties
+        //public string nim { get; set; }
+        //public string nama { get; set; }
+        //public double ipk { get; set; }
+
+        public string nim;
+        public string nama;
+        public float ipk;
+
+        public Mahasiswa(string nim, string nama, float ipk) {
+            this.nim = nim;
+            this.nama = nama;
+            this.ipk = ipk;
+        }
+
+        public void registrasi() {
+            Console.WriteLine("Nama : {0}", nama);
+            Console.WriteLine("NIM  : {0}", nim);
+            Console.WriteLine("IPK  : {0}", ipk);
+            Console.WriteLine("Telah melakukan registrasi!");
+        }
+
+        public void isiKrs() {
+            Console.WriteLine("{0} sedang mengisi KRS", nama);
+        }
+    }
+}
---
+++ Class dan Object/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_dan_Object {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Mahasiswa saya = new Mahasiswa("18.11.3344", "Andi", 3.20f);
+            saya.registrasi();
+            saya.isiKrs();
+
+            Console.WriteLine("\n\n");
+
+            Mahasiswa kamu = new Mahasiswa("18.11.1234", "Reno", 3.50f);
+            kamu.registrasi();
+            kamu.isiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
